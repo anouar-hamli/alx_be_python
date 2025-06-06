434af2f9cfd811e374ce304bd66cf7545fdb0568
anouar hamli <anouarhamli10@gmail.com>
2025-06-06 09:13:34 +0100

    commit

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,6 +1,7 @@
+def perform_operation(num1, num2, operation):
 
 
-def perform_operation(num1: float, num2: float, operation: str):
+
     if operation == 'add':
         return num1 + num2
     elif operation == 'subtract':
@@ -8,8 +9,9 @@ def perform_operation(num1: float, num2: float, operation: str):
     elif operation == 'multiply':
         return num1 * num2
     elif operation == 'divide':
+
         if num2 == 0:
-            return "Error: Division by zero"
+            return "Error: Cannot divide by zero"
         return num1 / num2
     else:
-        return "Error: Invalid operation"
+        return "Error: Unsupported operation"

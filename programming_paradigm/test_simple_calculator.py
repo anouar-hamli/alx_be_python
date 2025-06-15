626ee3e15862e9d28391451b6f02718cd45f7548
anouar hamli <anouarhamli10@gmail.com>
2025-06-15 19:46:02 +0100

    100%

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,45 +1,39 @@
 import unittest
 from simple_calculator import SimpleCalculator
 
-
 class TestSimpleCalculator(unittest.TestCase):
 
     def setUp(self):
-
         self.calc = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
         self.assertEqual(self.calc.add(0, 0), 0)
         self.assertEqual(self.calc.add(-5, -7), -12)
-        self.assertEqual(self.calc.add(1.5, 2.5), 4.0)
+        self.assertAlmostEqual(self.calc.add(2.5, 3.1), 5.6)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(5, 3), 2)
         self.assertEqual(self.calc.subtract(3, 5), -2)
-        self.assertEqual(self.calc.subtract(-1, -1), 0)
+        self.assertEqual(self.calc.subtract(-2, -3), 1)
         self.assertEqual(self.calc.subtract(0, 0), 0)
-        self.assertEqual(self.calc.subtract(2.5, 1.5), 1.0)
+        self.assertAlmostEqual(self.calc.subtract(5.5, 2.2), 3.3)
 
     def test_multiply(self):
         self.assertEqual(self.calc.multiply(4, 5), 20)
-        self.assertEqual(self.calc.multiply(-1, 5), -5)
-        self.assertEqual(self.calc.multiply(0, 100), 0)
-        self.assertEqual(self.calc.multiply(-3, -6), 18)
-        self.assertEqual(self.calc.multiply(2.5, 4), 10.0)
+        self.assertEqual(self.calc.multiply(-4, 5), -20)
+        self.assertEqual(self.calc.multiply(-4, -5), 20)
+        self.assertEqual(self.calc.multiply(4, 0), 0)
+        self.assertAlmostEqual(self.calc.multiply(2.5, 4), 10.0)
 
     def test_divide(self):
         self.assertEqual(self.calc.divide(10, 2), 5)
-        self.assertEqual(self.calc.divide(-10, 2), -5)
         self.assertEqual(self.calc.divide(10, -2), -5)
         self.assertEqual(self.calc.divide(-10, -2), 5)
-        self.assertEqual(self.calc.divide(5, 2), 2.5)
+        self.assertEqual(self.calc.divide(0, 5), 0)
+        self.assertAlmostEqual(self.calc.divide(5.5, 2.0), 2.75)
+        self.assertIsNone(self.calc.divide(10, 0))  # Division by zero returns None
 
-
-        self.assertIsNone(self.calc.divide(10, 0))
-        self.assertIsNone(self.calc.divide(0, 0))
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
